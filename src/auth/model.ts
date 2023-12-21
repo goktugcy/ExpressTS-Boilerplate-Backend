e3bd17f435910a91d49da5a6f3656f7a4c705f95
Göktuğ Ceyhan <goktugceyhan@gmail.com>
2023-12-21 20:33:15 +0300

    reset password minor fix

--- src/auth/model.ts
+++ src/auth/model.ts
@@ -2,6 +2,6 @@ import mongoose from 'mongoose'
 import { userSchema, passwordResetSchema } from './schema'
 
 const User = mongoose.model('User', userSchema)
-const PasswordReset = mongoose.model('PasswordReset', passwordResetSchema)
+const PasswordReset = mongoose.model('password_reset', passwordResetSchema)
 
 export { User, PasswordReset }
